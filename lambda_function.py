c2ece6381eb2346afa28d898046172b6ab238aa9
Tsubasa Ogawa <hurikake@gmail.com>
2019-04-27 08:29:08 +0000

    make it possible to get the maximum precip prob in today

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import os
+import pytz
+from datetime import datetime
 import linebot_publisher
 
 
@@ -10,26 +12,43 @@ class Weather:
 
   def __init__(self):
     self.dark_sky_api_key = os.environ['DARK_SKY_API_KEY']
+    tz = pytz.timezone('Asia/Tokyo')
+    today_12am = int(datetime.now(tz).replace(hour=0, minute=0, second=0, microsecond=0).strftime('%s'))
+    self.tomorrow_12am = today_12am + 86400
 
   def set_geo(self, **kwargs):
     self.lat = kwargs['lat']
     self.lon = kwargs['lon']
 
-  def get_precip_probability(self):
-    response_today = self._get_response_today()
-    # status = self._get_status(response_today)
-
-    if not 'precipType' in response_today:
+  def get_max_precip_probability(self):
+    precip_probs = self._get_precip_probabilities()
+    if not precip_probs:
       return 0
 
-    return round(response_today['precipProbability'] * 100)
+    print('Obtained probabilities: {0}'.format(precip_probs))
 
-  def _get_response_today(self):
+    max_precip_prob = max(precip_probs)
+    # status = self._get_status(response_today)
+
+    return round(max_precip_prob * 100)
+
+  def _get_precip_probabilities(self):
     uri = '{0}/{1}/{2},{3}'.format(
         self.DARK_SKY_BASE_URL, self.dark_sky_api_key,
         self.lat, self.lon
     )
-    return requests.get(uri).json()['daily']['data'][0]
+    responses = requests.get(uri).json()['hourly']['data']
+
+    precip_probs = []
+    for i, response in enumerate(responses):
+      if response['time'] >= self.tomorrow_12am:
+        break
+      elif 'precipType' not in response:
+        continue
+      precip_probs.append(response['precipProbability'])
+
+    print(precip_probs)
+    return precip_probs
 
   def _get_status(self, response):
     return response['icon']
@@ -39,8 +58,9 @@ def lambda_handler(event={}, context={}):
   weather = Weather()
   publisher = linebot_publisher.LineBotPublisher()
   weather.set_geo(lat=35.7004, lon=139.805)
-  prob = weather.get_precip_probability()
-  if prob > 0 and os.environ['CAN_POST_TO_LINE']:
+  prob = weather.get_max_precip_probability()
+  print("Today's max precip probability is {0}%".format(prob))
+  if prob > int(os.environ['PRECIP_THRESHOLD_PERCENT']) and os.environ['CAN_POST_TO_LINE']:
     publisher.post_text(
       os.environ['LINE_BOT_TO_ID'],
       '今日は雨が降りそう (降水確率: {0}%)'.format(prob)
